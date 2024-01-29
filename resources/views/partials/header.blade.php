1524c83bd4af96b7ab442c21be7c9148233d09d2
vitodursopettersson <vitodurso98@gmail.com>
2024-01-29 18:48:35 +0100

    header structure

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,69 @@
+<header>
+
+    <!-- Topbar -->
+    <section id="header-top-bar">
+        <div class="container">
+            <a href="#">DC power Visa</a>
+            <a href="#">additional DC sities</a>
+        </div>
+    </section>
+
+    <!-- Navbar -->
+    <nav>
+        <div class="container d-flex justify-content-between">
+
+            <!-- Brand -->
+            <div class="brand">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+            </div>
+
+            <!-- Menu -->
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Charaters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop</a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- Search -->
+            <div class="search">
+                <div class="search-bar">
+                    <input type="text" placeholder="Search">
+                    <div class="search-icon">
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</header>
