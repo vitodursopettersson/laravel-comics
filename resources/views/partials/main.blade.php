b2ba31377ad51dc504ebf37efb1fd85fd51f28d3
vitodursopettersson <vitodurso98@gmail.com>
2024-02-01 18:10:38 +0100

    feat: banner menu

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -36,7 +36,33 @@
                 <button class="btn-load-more">Load more</button>
 
             </div>
+        </div>
 
+    </section>
+
+    <!-- Banner Menu -->
+    <section id="banner-menu">
+        <div class="banner">
+            <a class="banner-menu-item" href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}">
+                <h6>digital comics</h6>
+            </a>
+            <a class="banner-menu-item" href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}">
+                <h6>dc merchandise</h6>
+            </a>
+            <a class="banner-menu-item" href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.pn') }}g">
+                <h6>subscription</h6>
+            </a>
+            <a class="banner-menu-item" href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}">
+                <h6>comic shop locator</h6>
+            </a>
+            <a class="banner-menu-item" href="#">
+                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}">
+                <h6>dc power visa</h6>
+            </a>
         </div>
     </section>
 
